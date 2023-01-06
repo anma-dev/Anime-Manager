8b3ba96f81a45641f287e1b1361160e931a06f1d
Anma Dev <121255032+anma-dev@users.noreply.github.com>
2023-01-06 13:34:49 +0100

    Fix autoinstall and onboarding.

--- utils/install-webtorrent.sh
+++ utils/install-webtorrent.sh
@@ -1,24 +1,37 @@
 #!/usr/bin/env bash
 
-# neither nvm nor node are installed at this point
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+if ! which nvm; then
+    if ! which node; then
+        # neither nvm nor node are installed at this point
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+        export NVM_DIR="$HOME/.nvm"
+        [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+        [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
-if ! grep 'export NVM_DIR="$HOME/.nvm"' "$HOME/.bash_profile"; then
-    printf "%b" '
+        if ! grep 'export NVM_DIR="$HOME/.nvm"' "$HOME/.bash_profile"; then
+            printf "%b" '
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 ' >>"$HOME/.bash_profile"
+        fi
+        # install node with nvm
+        # ensure we are using the latest version
+        nvm install node
+        nvm use node
+    else
+        # respect the user installation
+        node_v="$(node -v | awk -F"." '{print $1}' | sed 's,v,,g')"
+        if [[ $node_v -lt 14 ]]; then
+            # tell the user to upgrade manually
+            install_dep_msg="🎉 Finished the autoinstall!\nNEXT STEPS: manually update your Node.js version to node>=14 and restart your Terminal to start using Anime Manager."
+        fi
+    fi
+else
+    # ensure we are using the latest version
+    nvm install node
+    nvm use node
 fi
-
-# install node with nvm
-# ensure we are using the latest version
-nvm install node
-nvm use node
-
 if [[ ! -d lib/webtorrent-cli ]]; then
     git clone https://github.com/anma-dev/webtorrent-cli.git lib/webtorrent-cli
     cd lib/webtorrent-cli || exit
