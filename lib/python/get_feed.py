131d8ebbc3347414436d7966f2c18e8f95f3085e
Anma Dev <121255032+anma-dev@users.noreply.github.com>
2023-02-27 18:28:00 +0100

    improve handling feed exceptions

--- lib/python/get_feed.py
+++ lib/python/get_feed.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import io
+from urllib import error as urllib_errors
+import traceback
 import json
 import argparse
 import feedparser
@@ -30,38 +32,49 @@ logger = Logger(file="get_feed.log",
                 debug=args.debug,
                 log_name="get_feed").log
 try:
-    error = False
     parsed_link = io.StringIO(args.link).getvalue()
-
     if args.etag != 0:
         feed_content = feedparser.parse(parsed_link, etag=args.etag)
     elif args.modified != 0:
         feed_content = feedparser.parse(parsed_link, modified=args.modified)
     else:
         feed_content = feedparser.parse(parsed_link)
-
+    # in this order
+    if feed_content.bozo:
+        if hasattr(feed_content, 'bozo_exception'):
+            raise feed_content.bozo_exception
+        else:
+            err_msg = "The feed contains non-well-formed content."
+            raise AssertionError(err_msg)
     if feed_content.status == HTTPStatus.NOT_MODIFIED:
         logger.info("Not modified")
         sys.exit(0)
-
-    if feed_content.bozo:
-        error = True
-        err_msg = "The feed contains non-well-formed content."
-        logger.error(err_msg)
-    if feed_content.status != HTTPStatus.OK:
-        error = True
-        err_msg = f"Unable to get feed content. Status code {feed_content.status}."
-    if not feed_content.etag and not feed_content.updated_parsed:
-        error = True
-        err_msg = "Unable to operate. Feed server returned no etag and last-modified date."
-    if error:
-        print(json.dumps({
-            "am_ret_code": codes.BAD_FEED,
-            "am_ret_msg": err_msg
-        }))
-        sys.exit(0)  # code 0 to not trigger the main script
+    if not feed_content.etag and not feed_content.updated:
+        err_msg = "Server replied with no etag and updated date."
+        raise AssertionError(err_msg)
+except AssertionError as err:
+    err_msg = json.dumps({
+        "am_ret_code": codes.BAD_FEED,
+        "am_ret_msg": f"{err}"
+    })
+    logger.error(err_msg)
+    print(err_msg)
+    sys.exit(0)  # code 0 to not trigger the main script
+except (urllib_errors.URLError, urllib_errors.HTTPError) as err:
+    logger.error(traceback.format_exc())
+    err_msg = json.dumps({
+        "am_ret_code": codes.CONN_ISSUE,
+        "am_ret_msg": f"An error occurred while requesting the feed."
+    })
+    print(err_msg)
+    sys.exit(0)  # code 0 to not trigger the main script
 except Exception as err:
-    logger.error(err)
-    sys.exit(1)
+    logger.error(traceback.format_exc())
+    err_msg = json.dumps({
+        "am_ret_code": codes.UNKNOWN,
+        "am_ret_msg": f"An unknown error occurred."
+    })
+    print(err_msg)
+    sys.exit(0)  # code 0 to not trigger the main script
 else:
     print(json.dumps(feed_content, ensure_ascii=False))
